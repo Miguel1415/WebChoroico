6a1d86970b41af36ee9f959847dc1501057ab40c
Miguel1415 <mecol_28@hotmail.com>
2025-12-29 21:30:27 -0300

    TSK-002-Implementacion_CI_CD - change index to app

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ print("Iniciando aplicación en Vercel...")
 # Vercel busca una variable llamada 'app' por defecto
 app = create_app(ProductionConfig)
 
-# Sanity check for Vercel
+# Test routes to debug 404
+@app.route('/')
+def v_home():
+    return "Home direct from index.py"
+
 @app.route('/vercel-test')
 def vercel_test():
     return "Vercel connects to index.py successfully!"

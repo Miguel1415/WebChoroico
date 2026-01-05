d18a7958c3355456e3cfbc4ef3f288e94b51fd0b
Miguel1415 <mecol_28@hotmail.com>
2026-01-05 12:39:51 -0300

    TSK-006-Noticias_Rediseno_Profesional - Diseno_Noticias

--- core/routes.py
+++ core/routes.py
@@ -30,10 +30,48 @@ def about_us():
 def news():
     """
     Ruta de 'Noticias'.
-    Reutiliza about_us.html como plantilla genérica por ahora.
+    Muestra un feed de noticias institucionales con diseño profesional.
     """
-    # TSK-001: Estandarización a Inglés
-    return render_template('about_us.html', section="Noticias", icon="fa-newspaper")
+    # TSK-006: Datos de ejemplo para el feed de noticias
+    news_items = [
+        {
+            'id': 1,
+            'title': 'Inauguración del Año Escolar 2025',
+            'excerpt': 'Damos la bienvenida a nuestros estudiantes para un nuevo ciclo de aprendizaje y crecimiento integral.',
+            'date': '05 Mar 2025',
+            'category': 'Evento',
+            'image': 'noticia_escuela_1.jpg',
+            'icon': 'fa-graduation-cap'
+        },
+        {
+            'id': 2,
+            'title': 'Éxito en la Feria Científica Regional',
+            'excerpt': 'Nuestros alumnos de 8vo básico obtuvieron el primer lugar con su proyecto de sustentabilidad.',
+            'date': '20 Nov 2024',
+            'category': 'Académico',
+            'image': 'noticia_escuela_2.jpg',
+            'icon': 'fa-flask'
+        },
+        {
+            'id': 3,
+            'title': 'Nueva Alianza de Deportes Choroico',
+            'excerpt': 'Firmamos un convenio para fomentar el deporte y la vida sana entre nuestros jóvenes deportistas.',
+            'date': '15 Nov 2024',
+            'category': 'Deportes',
+            'image': 'noticia_escuela_3.jpg',
+            'icon': 'fa-medal'
+        },
+        {
+            'id': 4,
+            'title': 'Taller de Robótica y Programación',
+            'excerpt': 'Iniciamos nuevos talleres tecnológicos para desarrollar las habilidades del siglo XXI en nuestros estudiantes.',
+            'date': '10 Nov 2024',
+            'category': 'Tecnología',
+            'image': 'noticia_escuela_4.jpg',
+            'icon': 'fa-robot'
+        }
+    ]
+    return render_template('news.html', section="Noticias", icon="fa-newspaper", news=news_items)
 
 @main.route('/gallery')
 def gallery():
